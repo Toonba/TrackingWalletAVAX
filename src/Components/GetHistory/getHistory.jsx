b84a9703cef5fdbcbd7c33818eef77502888c729
Aymerick Toonba <godzilla.toonba@gmail.com>
2023-04-20 16:31:54 +0200

    obtiens les balances de chaque jours pour des range de 7, 30 120 jours

--- src/Components/GetHistory/getHistory.jsx
+++ src/Components/GetHistory/getHistory.jsx
@@ -7,7 +7,6 @@ function GetHistory() {
   // 27639779
   const inputValue = useSelector((state) => state.inputValue)
   const today = new Date()
-  const targetDate = new Date()
   const sevenDay = new Date()
   const thirtyDay = new Date()
   const hundredDay = new Date()
@@ -15,33 +14,45 @@ function GetHistory() {
   sevenDay.setMilliseconds(today.getMilliseconds() + 10)
   thirtyDay.setDate(today.getDate() - 30)
   thirtyDay.setMilliseconds(today.getMilliseconds() + 10)
-  hundredDay.setDate(today.getDate() - 100)
+  hundredDay.setDate(today.getDate() - 365)
   hundredDay.setMilliseconds(today.getMilliseconds() + 10)
 
-  targetDate.setDate(today.getDate() - 7)
-  targetDate.setMilliseconds(today.getMilliseconds() + 10)
-  const dateToTrack = [sevenDay, thirtyDay, hundredDay]
+  // const dateToTrack = [sevenDay, thirtyDay, hundredDay]
   const [myData, setMyData] = useState([{ address: inputValue, balance: 0, date: null }])
-  const [test, setTest] = useState([])
+  const [selectedOption, setSelectedOption] = useState(7)
 
-  const addElement = (newElement) => {
-    const newArray = [...test]
-    newArray.push(newElement)
-    setTest(newArray)
+  const targetDay = new Date()
+  targetDay.setDate(today.getDate() - selectedOption)
+  const dateToTrack = getDatesBetween(targetDay, today)
+
+  const handleSelectChange = (event) => {
+    setSelectedOption(event.target.value)
   }
 
   useEffect(() => {
-    const fetchData = async (date) => {
-      const blockNumber = await getBlockNumberForDates(date)
-      const result = await getBalance(inputValue, blockNumber)
-      addElement({ balance: result.balance, date: result.date })
+    const fetchData = async () => {
+      const promises = dateToTrack.map(async (date) => {
+        const blockNumber = await getBlockNumberForDates(date)
+        const result = await getBalance(inputValue, blockNumber)
+        return { balance: result.balance, date: result.date }
+      })
+      const balances = await Promise.all(promises)
+      setMyData(balances)
     }
-    dateToTrack.forEach((element) => fetchData(element))
-  }, [inputValue])
+    fetchData()
+  }, [inputValue, targetDay])
 
   return (
-    <div>
-      {test.map((element, index) => (
+    <div className="previousBalance">
+      <div className="whichRange">
+        <label for="chooseRange">Which range do you want to check ?</label>
+        <select name="range" id="chooseRange" value={selectedOption} onChange={handleSelectChange}>
+          <option value="7">7 days</option>
+          <option value="30">30 days</option>
+          <option value="120">120 days</option>
+        </select>
+      </div>
+      {myData.map((element, index) => (
         <p key={`${element}-${index}`}>{`Il était de ${element.balance} AVAX le ${element.date}`}</p>
       ))}
     </div>
--- src/Service/web3.js
+++ src/Service/web3.js
@@ -3,12 +3,23 @@ import Web3 from 'web3'
 const avalancheProvider = 'https://api.avax.network/ext/bc/C/rpc'
 const web3 = new Web3(avalancheProvider)
 
+/**
+ * 
+ * @param {number} blockNumber block number for which you want to check date 
+ * @returns {Date} of block checked 
+ */
 async function getBlockDate(blockNumber) {
   const block = await web3.eth.getBlock(blockNumber)
   const blockDate = new Date(block.timestamp * 1000)
   return blockDate
 }
 
+/**
+ * 
+ * @param {Date} date1 
+ * @param {Date} date2 order does not matter
+ * @returns {number} number of day between both date
+ */
 function diffDays(date1, date2) {
   const diffTime = Math.abs(date1.getTime() - date2.getTime())
   const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24))
@@ -51,21 +62,25 @@ export async function getBalance(inputValue, blockNumber = null) {
   const address = inputValue
   const balanceInWei = await web3.eth.getBalance(address, blockNumber)
   const balanceInAVAX = balanceInWei / 1e18
+  const balance = balanceInAVAX.toFixed(2)
   let blockDate = await getBlockDate(blockNumber)
-  return { address: address, balance: balanceInAVAX, date: getDateJJMMYYY(blockDate) }
+  return { address: address, balance: balance, date: getDateJJMMYYY(blockDate) }
 }
 
 export async function getBlockNumberForDates(targetDate) {
   const today = new Date()
   const diffDaysValue = diffDays(today, targetDate)
   const latestBlockNumber = await web3.eth.getBlockNumber()
+  // 43000 is approximately the number of block there is each day it allow to be closer of the result from start 
   const supposedBlockNumberAtTargetDate = latestBlockNumber - diffDaysValue * 43000
   const block = await web3.eth.getBlock(supposedBlockNumberAtTargetDate)
   const blockDate = new Date(block.timestamp * 1000)
   const areDateEqual = isDateMatching(blockDate, targetDate)
+
   if (areDateEqual) {
     return supposedBlockNumberAtTargetDate
   } else {
+    // if supposed date isn't equal to target date binary search to find a block that match the target date 
     let startBlockNumber = supposedBlockNumberAtTargetDate
     let endBlockNumber = latestBlockNumber
     let blockNumber = -1
@@ -75,9 +90,9 @@ export async function getBlockNumberForDates(targetDate) {
       const block = await web3.eth.getBlock(midBlockNumber)
       const blockDate = new Date(block.timestamp * 1000)
 
-      if (blockDate < targetDate) {
+      if (blockDate.getFullYear() < targetDate.getFullYear() || (blockDate.getFullYear() === targetDate.getFullYear() && blockDate.getMonth() < targetDate.getMonth()) || (blockDate.getFullYear() === targetDate.getFullYear() && blockDate.getMonth() === targetDate.getMonth() && blockDate.getDate() < targetDate.getDate())) {
         startBlockNumber = midBlockNumber + 1
-      } else if (blockDate > targetDate) {
+      } else if (blockDate.getFullYear() > targetDate.getFullYear() || (blockDate.getFullYear() === targetDate.getFullYear() && blockDate.getMonth() > targetDate.getMonth()) || (blockDate.getFullYear() === targetDate.getFullYear() && blockDate.getMonth() === targetDate.getMonth() && blockDate.getDate() > targetDate.getDate())) {
         endBlockNumber = midBlockNumber - 1
       } else {
         blockNumber = midBlockNumber
